769a0b94e96cd3a3ae5977f4f1546465122ba12a
Michael Risher <rishermichael@gmail.com>
2014-02-19 14:43:48 -0800

    assignment 1:j

---
+++ assignment 1/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on February 19, 2014, 1:36 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
