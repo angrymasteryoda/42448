51e83cb50b63b2b6a0c9fe9ac8cd0152e529a6e1
Michael Risher <michael.risher@omniplatform.com>
2014-02-20 15:33:54 -0800

    array pointers

--- assignment1/main.cpp
+++ assignment1/main.cpp
@@ -14,81 +14,93 @@ using namespace std;
 /*
  * 
  */
-void printArr( int[], int, int );
-void fillArr( int[], int );
-void bubbleSort( int[], int );
+void printArr( int*, int, int );
+//void fillArr( int[], int );
+int *fillArr( int );
+void bubbleSort( int*, int );
 void swap1( int&, int& );
-int binarySearch( int[], int, int );
+int binarySearch( int*, int, int );
 
+int main( int argc, char** argv ) {
+    ///*
+    srand( time( NULL ) );
 
-int main(int argc, char** argv) {
-    srand(time(NULL));
-    
     int size = 100;
-    int arr[size];
-    
-    fillArr( arr, size );
-    printArr( arr, size, 10);
-    
+//    int arr[size];
+    int *arr = fillArr( size );
+
+    cout << "your array\n";
+    //fillArr( arr, size );
+    printArr( arr, size, 10 );
+
     bubbleSort( arr, size );
 
-    printArr( arr, size, 10);
+    cout << "your array\n";
+    printArr( arr, size, 10 );
+
+    cout << "Binary search for what value? ";
+    short qval;
+    cin >> qval;
+
+    cout << "Its at index " << binarySearch( arr, size, qval );
+    //*/
     return 0;
 }
 
-void fillArr( int a[], int s){
-    for( int i = 0; i < s; i++){
-        a[i] = rand() % 90 + 10;
+int *fillArr( int s ) {
+    int *a = new int[s];
+    for ( int i = 0; i < s; i++ ) {
+        *(a + i)= rand( ) % 90 + 10;
     }
-}
-
-void printArr( int a[], int s, int perLine){
-    cout << endl;
-    for( int i = 0; i < s; i++){
-        cout << a[i] <<" ";
-        if( i % 10 == ( perLine - 1 ) ) cout << endl;
-    }
-    cout << endl;
-}
-
-void bubbleSort( int arr[], int s ){
-    bool swap = false;
     
-    do{
-        
-        for( int i = 0; i < s - 1; i++ ){
-            if ( arr[i] > arr[i + 1] ){
-                swap1( arr[i], arr[i + 1] );
+    return a;
+}
+
+void printArr( int *a, int s, int perLine ) {
+    cout << endl;
+    for ( int i = 0; i < s; i++ ) {
+        cout << *(a + i) << " ";
+        if ( i % 10 == ( perLine - 1 ) ) cout << endl;
+    }
+    cout << endl;
+}
+
+void bubbleSort( int *arr, int s ) {
+    bool swap = false;
+
+    do {
+        swap = false;
+        for ( int i = 0; i < s - 1; i++ ) {
+            if ( *(arr+i) > *(arr+(i + 1)) ) {
+                swap1( *(arr + i), *(arr + (i + 1) ) );
+                swap = true;
             }
 
         }
-    }
-    while( swap );
+    }    while ( swap );
 }
 
-int binarySearch(int array[], int size, int val) {
-   int first = 0, last = size - 1;
-   int mid;
-   
-   bool found = false;
+int binarySearch( int *arr, int size, int val ) {
+    int first = 0, last = size - 1;
+    int mid;
 
-   while (!found && first <= last) {
-      mid = (first + last) / 2;
-      if ( array[mid] == val ){
-         found = true;
-         return mid;
-      }
-      else if ( array[mid] > val ) {
-         last = mid - 1;
-      }
-      else {
-         first = mid + 1;
-      }
-   }
-   return -1;
-} 
+    bool found = false;
 
-void swap1( int &n1, int &n2 ){
+    while ( !found && first <= last ) {
+        mid = ( first + last ) / 2;
+        if ( *(arr + mid) == val ) {
+            found = true;
+            return mid;
+        } else if ( *(arr + mid) > val ) {
+            last = mid - 1;
+        } else {
+            first = mid + 1;
+        }
+    }
+    return -1;
+}
+
+void swap1( int &n1, int &n2 ) {
     n1 ^= n2;
     n2 = n1 ^ n2;
     n1 ^= n2;
