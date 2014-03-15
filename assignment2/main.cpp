cb9309e1fc41f53c207c79fd2d6c7eac8bc801e1
Michael Risher <rishermichael@gmail.com>
2014-03-14 17:44:20 -0700

    mode work done

--- assignment2/main.cpp
+++ assignment2/main.cpp
@@ -34,12 +34,57 @@ void destroy(int *);
 void meanMedianMode();
 void mode( int *a, int size);
 
+//problems
+void Menu();
+int getN();
+void problem1();
+void problem2();
+void problem3();
+float dropLowest( int *arr, int s);
+void problem4();
+void problem5();
+void problem6();
+int* reverse(int *a, int s);
+void problem7();
+int* doubleArray( int *a, int s );
+void problem8();
+int* shiftedArray( int *a, int s );
+void problem9();
+int doSomething(int *x, int *y);
+
 int main( int argc, char** argv ) {
    
     srand( time( NULL ) );
     
-    meanMedianMode();
+    int inN;
+    do{
+        Menu();
+        inN=getN();
+        switch(inN){
+            case 1:    problem1();break;
+            case 2:    problem2();break;
+            case 3:    problem3();break;
+            case 4:    problem4();break;
+            case 5:    problem5();break;
+            case 6:    problem6();break;
+            case 7:    problem7();break;
+            case 8:    problem8();break;
+            case 9:    problem9();break;
+            case 10:   meanMedianMode();break;
+            default:   exit(1);
+        }
+    } while(inN<11);    
     return 0;
+}
+
+void Menu() {
+	cout<<"Type 1-10 for problem 1-10"<<endl;
+	cout<<"Type 11 to exit \n"<<endl;
+}
+int getN() {
+	int inN;
+	cin>>inN;
+	return inN;
 }
 
 void meanMedianMode(){
@@ -108,6 +153,7 @@ void mode( int *a, int size){
         if ( i <= b[0] ) { cout << ", "; }
         else { cout << endl; }
     }
+    cout << endl;
     destroy(b);
 }
 
@@ -255,3 +301,193 @@ int *copy(const int *a,int n){
 void destroy(int *a){
     delete [] a;
 }
+
+
+void problem1(){
+    cout << "enter a size for array: ";
+    int size;
+    cin >> size;
+    
+    int *arr = fillArray( size );
+    cout << endl << &arr << endl;
+    
+    destroy( arr );
+}
+
+void problem2(){
+    cout << "Entering test scores from 10 - 30\n";
+    int size = rand() % 20 + 10;
+    
+    int *arr = fillArray( size );
+    
+    bubbleSort( arr, size );
+    
+    cout << "test scores\n";
+    printArr( arr, size, 10);
+    cout << "average: " << mean( arr, size ) << endl;
+    
+    destroy( arr );
+}
+
+float dropLowest( int *arr, int s){
+    
+    int *res = copy( arr, s );
+    int offset = 0;
+    
+    if ( arr[0] < arr[1] ) {
+        offset = 1;
+    }
+    else if ( arr[0] == arr[1] ) {
+        for(int i = 1; i < s - 1; i++ ){
+            if ( arr[i] < arr[i + 1] ) {
+                i = s;
+            }
+            else if ( arr[i] == arr[i + 1] ) {
+                offset++;
+            }
+        }
+    }
+    
+    float result = 0;
+    
+    for( int i = offset; i < s; i++ ){
+        result += (float) *( arr + i );
+    }
+    
+    result /= (float) s - offset;
+    return result;         
+}
+
+void problem3(){
+        cout << "Entering test scores from 10 - 30\n";
+    int size = rand() % 20 + 10;
+    
+    int *arr = fillArray( size );
+    
+    bubbleSort( arr, size );
+    
+    cout << "test scores\n";
+    printArr( arr, size, 10);
+    cout << "average: " << dropLowest( arr, size ) << endl;
+    
+    destroy( arr );
+}
+void problem4(){
+//lazy problem 8
+    cout << "enter array size\n";
+    int s;
+    cin >> s;
+    cout << "your array\n";
+    int *a = fillArray( s );
+    bubbleSort( a, s);
+    printArr( a, s, 10);
+    cout << "mode\n";
+    mode( a, s );
+    destroy( a );
+
+}
+void problem5(){
+//lazy problem 9
+    cout << "enter array size\n";
+    int s;
+    cin >> s;
+    cout << "your array\n";
+    int *a = fillArray( s );
+    bubbleSort( a, s);
+    printArr( a, s, 10);
+    cout << "median: " << median( a, s ) << endl;
+    destroy( a );
+}
+void problem6(){
+//problem 10
+    int s = rand() % 20 + 2;
+    cout << "your array\n";
+    int *a = fillArray( s );
+    printArr( a, s, 10 );
+    
+    int *b = reverse( a, s );
+    cout << "reversed\n";
+    printArr( b, s, 10 );
+    
+    destroy( b );
+    destroy( a );
+}
+
+int* reverse(int *a, int s){
+    int *copyA = copy( a, s );
+    int *b = new int[s];
+    for( int i = 0; i < s; i++){
+        b[(s - 1) - i] = copyA[i];
+    }
+    destroy( copyA );
+    return b;
+}
+void problem7(){
+//problem 11
+    int s = rand() % 20 + 2;
+    cout << "your array\n";
+    int *a = fillArray( s );
+    printArr( a, s, 10 );
+    
+    int *b = doubleArray( a, s );
+    cout << "doubled\n";
+    printArr( b, s * 2, 10 );
+    
+    destroy( b );
+    destroy( a );
+}
+
+int* doubleArray( int *a, int s ){
+    int *copyA = copy( a, s );
+    int newS = s * 2;
+    int *b = new int[newS];
+    for( int i = 0; i < s; i++){
+        b[i] = copyA[i];
+    }
+    for( int i = 0; i < s; i++){
+        b[s + i] = copyA[i];
+    }
+    destroy( copyA );
+    return b;
+}
+void problem8(){
+//problem 11
+    int s = rand() % 20 + 2;
+    cout << "your array\n";
+    int *a = fillArray( s );
+    printArr( a, s, 10 );
+    
+    int *b = shiftedArray( a, s );
+    cout << "shifted\n";
+    printArr( b, s + 1, 10 );
+    
+    destroy( b );
+    destroy( a );
+}
+int* shiftedArray( int *a, int s ){
+    int *copyA = copy( a, s );
+    int newS = s + 1;
+    int *b = new int[newS];
+    b[0] = 0;
+    for( int i = 1; i < newS; i++){
+        b[i] = copyA[i - 1];
+    }
+    destroy( copyA );
+    return b;
+}
+void problem9(){
+    int x = 1, y = 2;
+    int *px = &x, *py = &y;
+    cout << "x: " << x << " y: " << y << " x + y: " << x + y << endl;
+    cout << "did a function\n";
+    cout << "x: " << x << " y: " << y << " x + y: " << doSomething( px, py ) << endl;
+}
+
+int doSomething(int *x, int *y) {
+    int temp = *x;
+    *x = *y * 10;
+    *y = temp * 10;
+    return *x + *y;
+}
+
+
